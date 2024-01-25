f844d066cdd1af5804c1d5a33d6ed4347a4b6cbe
MC <github@mcz.io>
2024-01-25 16:38:28 +0000

    Made vtable generic

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use libloading::os::unix::Symbol as RawSymbol;
 use libloading::{Library, Symbol};
 
 #[repr(C)]
-struct Object {
+pub struct Object {
     _private: [u8; 0],
 }
 type FreeObject = extern "C" fn(*mut Object);
@@ -14,14 +14,19 @@ type GetApiVersion = extern "C" fn() -> c_int;
 type GetInfo = extern "C" fn(*const Object) -> c_int;
 type SetInfo = extern "C" fn(*mut Object, c_int);
 
+pub trait VTable {
+    unsafe fn new(library: &Library) -> Self;
+    unsafe fn free(&self, object: *mut Object);
+}
+
 struct VTableV0 {
     free_object: RawSymbol<FreeObject>,
     get_info: RawSymbol<GetInfo>,
     set_info: RawSymbol<SetInfo>,
 }
 
-impl VTableV0 {
-    unsafe fn new(library: &Library) -> VTableV0 {
+impl VTable for VTableV0 {
+    unsafe fn new(library: &Library) -> Self {
         println!("Loading API version 0...");
         let free_object: Symbol<FreeObject> = library.get(b"free_object\0").unwrap();
         let free_object = free_object.into_raw();
@@ -31,26 +36,30 @@ impl VTableV0 {
         let set_info = set_info.into_raw();
 
         VTableV0 {
-            free_object: free_object,
-            get_info: get_info,
-            set_info: set_info,
+            free_object,
+            get_info,
+            set_info,
         }
+    }
+    unsafe fn free(&self, object: *mut Object)
+    {
+        (&self.free_object)(object);
     }
 }
 
-struct Plugin {
+struct Plugin<T: VTable> {
     #[allow(dead_code)]
     library: Library,
     object: *mut Object,
-    vtable: VTableV0,
+    vtable: T,
 }
 
-impl Plugin {
-    unsafe fn new(library_name: &OsStr) -> Plugin {
+impl<T: VTable> Plugin<T>{
+    unsafe fn new(library_name: &OsStr) -> Plugin<T> {
         let library = Library::new(library_name).unwrap();
         let get_api_version: Symbol<GetApiVersion> = library.get(b"get_api_version\0").unwrap();
         let vtable = match get_api_version() {
-            0 => VTableV0::new(&library),
+            0 => T::new(&library),
             _ => panic!("Unrecognized C API version number."),
         };
 
@@ -58,22 +67,25 @@ impl Plugin {
         let object: *mut Object = init();
 
         Plugin {
-            library: library,
-            object: object,
-            vtable: vtable,
+            library,
+            object,
+            vtable,
         }
     }
 }
 
-impl Drop for Plugin {
+impl<T: VTable> Drop for Plugin<T> {
     fn drop(&mut self) {
-        (self.vtable.free_object)(self.object);
+        unsafe
+        {
+            self.vtable.free(self.object);
+        }
     }
 }
 
 fn main() {
     let library_path: &OsStr = OsStr::new("ffi-test/libffi-test.so");
-    let plugin = unsafe { Plugin::new(library_path) };
+    let plugin = unsafe { Plugin::<VTableV0>::new(library_path) };
 
     println!(
         "Original value: {}",
